9ee29c110ccf053d3436b2d536fa410c5a2f3863
Joe_A <joe.alvarez42@gmail.com>
2022-08-24 19:13:19 -0600

    [updated file]
    created AuthPackage class, to handle authentication

--- src/botPython/tweepyAPI.py
+++ src/botPython/tweepyAPI.py
@@ -1,13 +1,23 @@
 import tweepy
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+class AuthPackage(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET):
+
+    # protected data members
+    def __init__(self):
+        self.__CONSUMER_KEY = CONSUMER_KEY
+        self.__CONSUMER_SECRET = CONSUMER_SECRET
+        self.__ACCESS_TOKEN = ACCESS_TOKEN
+        self.__ACCESS_TOKEN_SECRET = ACCESS_TOKEN_SECRET
+        pass
+
+        # Authenticate to Twitter
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
 
 # Create API object
 api = tweepy.API(auth)
 
 # Create a tweet
 api.update_status("Hello Tweepy")
-
-
